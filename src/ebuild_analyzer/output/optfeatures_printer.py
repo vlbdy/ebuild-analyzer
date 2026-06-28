bf8e462b9c122e0ee33ebe575fb201305120f580
Vlad B <vbadyorny@gmail.com>
2026-06-28 20:17:21 +0300

    output: Create and use an OutputBuffer

--- src/ebuild_analyzer/output/optfeatures_printer.py
+++ src/ebuild_analyzer/output/optfeatures_printer.py
@@ -1,8 +1,9 @@
 from collections import defaultdict
 from typing import List
 
-from ebuild_analyzer.output.ansi import Format, Color
 from ebuild_analyzer.extractor.optfeature import OptFeature, PackageWithUses
+from ebuild_analyzer.output.ansi import Format, Color
+from ebuild_analyzer.output.output_buffer import OutputBuffer
 from ebuild_analyzer.path_conditions.path_conditions import PathConditions
 from ebuild_analyzer.utils.portage_db import PortageDatabase
 
@@ -10,41 +11,41 @@ from ebuild_analyzer.utils.portage_db import PortageDatabase
 class OptFeaturesPrinter:
     def __init__(self, portage_db: PortageDatabase) -> None:
         self.__portage_db = portage_db
+        self.__buffer = OutputBuffer()
 
     def print_optfeatures(self, target_package: str, optfeatures: List[OptFeature]) -> None:
-        print(Color.LIGHT_PURPLE(Format.BOLD(f"Optional features for package {target_package}:")), end='')
+        self.__buffer.push(Color.LIGHT_PURPLE(Format.BOLD(f"Optional features for package {target_package}:\n")))
+        self.__buffer.push_indent()
 
         optfeatures_groups = defaultdict(list)
         for optfeature in optfeatures:
             optfeatures_groups[optfeature.header].append(optfeature)
 
         for header, optfeatures in optfeatures_groups.items():
-            print()
-            indentation = 0
             if header is not None:
-                print(Color.BLUE(Format.BOLD(header)))
-                indentation += 4
+                self.__buffer.indented_push(Color.BLUE(Format.BOLD(f"{header}\n")))
+                self.__buffer.push_indent()
 
             for optfeature in optfeatures:
-                print(f"{' ' * indentation}{Format.BOLD(optfeature.description)} ", end='')
+                self.__buffer.indented_push(Format.BOLD(optfeature.description))
                 self.__print_feature_availability(optfeature)
 
                 if optfeature.visibility_conditions:
-                    self.__print_optfeature_visibility_conditions(target_package, optfeature.visibility_conditions,
-                                                                  indentation)
-                self.__print_packages_required_to_enable_optfeature(optfeature.possible_feature_dependencies,
-                                                                    indentation)
-                print()
+                    self.__print_optfeature_visibility_conditions(target_package, optfeature.visibility_conditions)
+                self.__print_packages_required_to_enable_optfeature(optfeature.possible_feature_dependencies)
+                self.__buffer.push('\n')
+            self.__buffer.pop_indent()
+        self.__buffer.print()
 
     def __print_feature_availability(self, optfeature: OptFeature) -> None:
         is_feature_available = self.__is_feature_available(optfeature)
 
-        print(Color.BLUE('['), end='')
+        self.__buffer.push(Color.BLUE(" ["))
         if is_feature_available:
-            print(Color.GREEN(Format.BOLD("Available")), end='')
+            self.__buffer.push(Color.GREEN(Format.BOLD("Available")))
         else:
-            print(Color.RED(Format.BOLD("Not Available")), end='')
-        print(Color.BLUE(']'))
+            self.__buffer.push(Color.RED(Format.BOLD("Not Available")))
+        self.__buffer.push(Color.BLUE("]\n"))
 
     def __is_feature_available(self, optfeature: OptFeature) -> bool:
         for feature_dependencies in optfeature.possible_feature_dependencies:
@@ -62,99 +63,100 @@ class OptFeaturesPrinter:
                 return True
         return False
 
-    def __print_optfeature_visibility_conditions(self, target_package: str, visibility_conditions: PathConditions,
-                                                 base_indentation: int) -> None:
-        indentation = base_indentation + 4
-        if visibility_conditions.installed_packages:
-            print(f"{' ' * indentation}Advertised when the following packages are installed: [", end='')
-            self.__print_packages_list(visibility_conditions.installed_packages)
-            print(']')
+    def __print_optfeature_visibility_conditions(self, target_package: str,
+                                                 visibility_conditions: PathConditions) -> None:
+        with self.__buffer.scoped_indent():
+            if visibility_conditions.installed_packages:
+                self.__buffer.indented_push("Advertised when the following packages are installed: [")
+                self.__print_packages_list(visibility_conditions.installed_packages)
+                self.__buffer.push("]\n")
 
-        if visibility_conditions.uninstalled_packages:
-            print(f"{' ' * indentation}Advertised when the following packages are not installed: [", end='')
-            self.__print_packages_list(visibility_conditions.uninstalled_packages,
-                                       installed_color=Color.RED, uninstalled_color=Color.GREEN)
-            print(']')
+            if visibility_conditions.uninstalled_packages:
+                self.__buffer.indented_push("Advertised when the following packages are not installed: [")
+                self.__print_packages_list(visibility_conditions.uninstalled_packages,
+                                           installed_color=Color.RED, uninstalled_color=Color.GREEN)
+                self.__buffer.push("]\n")
 
-        if visibility_conditions.enabled_use_flags:
-            print(f"{' ' * indentation}Advertised when the following USE flags are enabled: [", end='')
-            self.__print_use_flags_to_enable_list(target_package, visibility_conditions.enabled_use_flags)
-            print(']')
+            if visibility_conditions.enabled_use_flags:
+                self.__buffer.indented_push("Advertised when the following USE flags are enabled: [")
+                self.__print_use_flags_to_enable_list(target_package, visibility_conditions.enabled_use_flags)
+                self.__buffer.push("]\n")
 
-        if visibility_conditions.disabled_use_flags:
-            print(f"{' ' * indentation}Advertised when the following USE flags are disabled: [", end='')
-            self.__print_use_flags_to_disable_list(target_package, visibility_conditions.disabled_use_flags)
-            print(']')
+            if visibility_conditions.disabled_use_flags:
+                self.__buffer.indented_push("Advertised when the following USE flags are disabled: [")
+                self.__print_use_flags_to_disable_list(target_package, visibility_conditions.disabled_use_flags)
+                self.__buffer.push("]\n")
 
-    def __print_packages_required_to_enable_optfeature(self, package_combinations: List[List[PackageWithUses]],
-                                                       base_indentation: int) -> None:
-        indentation = base_indentation + 4
-        print(f"{' ' * indentation}Required packages to enable the feature:")
-        for i, package_combo in enumerate(package_combinations):
-            self.__print_package_combination(package_combo, indentation)
+    def __print_packages_required_to_enable_optfeature(self, package_combinations: List[List[PackageWithUses]]) -> None:
+        with self.__buffer.scoped_indent():
+            self.__buffer.indented_push("Required packages to enable the feature:\n")
+            for i, package_combo in enumerate(package_combinations):
+                self.__print_package_combination(package_combo)
 
-            if i != len(package_combinations) - 1:
-                print(Format.BOLD(" or"))
-            else:
-                print()
+                if i != len(package_combinations) - 1:
+                    self.__buffer.push(Format.BOLD(" or\n"))
+                else:
+                    self.__buffer.push('\n')
 
     def __print_packages_list(self, packages: List[str], installed_color: Color = Color.GREEN,
                               uninstalled_color: Color = Color.RED) -> None:
         for i, package in enumerate(packages):
-            self.__print_colored_package_name(package, 0, installed_color, uninstalled_color)
+            self.__print_colored_package_name(package, installed_color, uninstalled_color)
 
             if i != len(packages) - 1:
-                print(', ', end='')
+                self.__buffer.push(', ')
 
     def __print_use_flags_to_disable_list(self, target_package: str, use_flags: List[str]) -> None:
         for i, use_flag in enumerate(use_flags):
             self.__print_colored_use_flag_to_disable(target_package, use_flag)
 
             if i != len(use_flags) - 1:
-                print(', ', end='')
+                self.__buffer.push(', ')
 
     def __print_use_flags_to_enable_list(self, target_package: str, use_flags: List[str]) -> None:
         for i, use_flag in enumerate(use_flags):
             self.__print_colored_use_flag_to_enable(target_package, use_flag)
 
             if i != len(use_flags) - 1:
-                print(', ', end='')
+                self.__buffer.push(', ')
 
-    def __print_package_combination(self, package_combo: List[PackageWithUses], base_indentation: int) -> None:
-        indentation = base_indentation + 4
-        for i, package in enumerate(package_combo):
-            self.__print_colored_package_name(package.package_name, indentation)
+    def __print_package_combination(self, package_combo: List[PackageWithUses]) -> None:
+        with self.__buffer.scoped_indent():
+            for i, package in enumerate(package_combo):
+                self.__buffer.indented_push("")
+                self.__print_colored_package_name(package.package_name)
 
-            if package.enabled_use_flags or package.disabled_use_flags:
-                print('[', end='')
-            if package.enabled_use_flags:
-                self.__print_use_flags_to_enable_list(package.package_name, package.enabled_use_flags)
+                if package.enabled_use_flags or package.disabled_use_flags:
+                    self.__buffer.push('[')
+                if package.enabled_use_flags:
+                    self.__print_use_flags_to_enable_list(package.package_name, package.enabled_use_flags)
+                    if package.disabled_use_flags:
+                        self.__buffer.push(', ')
                 if package.disabled_use_flags:
-                    print(', ', end='')
-            if package.disabled_use_flags:
-                self.__print_use_flags_to_disable_list(package.package_name, package.disabled_use_flags)
-            if package.enabled_use_flags or package.disabled_use_flags:
-                print(']', end='')
+                    self.__print_use_flags_to_disable_list(package.package_name, package.disabled_use_flags)
+                if package.enabled_use_flags or package.disabled_use_flags:
+                    self.__buffer.push(']')
 
-            if i != len(package_combo) - 1:
-                print(Format.BOLD(" and "), end='')
-                indentation = 0
+                if i != len(package_combo) - 1:
+                    self.__buffer.push(Format.BOLD(" and "))
+                    self.__buffer.disable_indentation()
+        self.__buffer.enable_indentation()
 
     def __print_colored_use_flag_to_disable(self, target_package: str, use_flag: str) -> None:
         if not self.__portage_db.is_use_flag_enabled(target_package, use_flag):
-            print(Color.GREEN(f"-{use_flag}"), end='')
+            self.__buffer.push(Color.GREEN(f"-{use_flag}"))
         else:
-            print(Color.RED(f"-{use_flag}"), end='')
+            self.__buffer.push(Color.RED(f"-{use_flag}"))
 
     def __print_colored_use_flag_to_enable(self, target_package: str, use_flag: str) -> None:
         if self.__portage_db.is_use_flag_enabled(target_package, use_flag):
-            print(Color.GREEN(use_flag), end='')
+            self.__buffer.push(Color.GREEN(use_flag))
         else:
-            print(Color.RED(use_flag), end='')
+            self.__buffer.push(Color.RED(use_flag))
 
-    def __print_colored_package_name(self, package: str, indentation: int, installed_color: Color = Color.GREEN,
+    def __print_colored_package_name(self, package: str, installed_color: Color = Color.GREEN,
                                      uninstalled_color: Color = Color.RED) -> None:
         if self.__portage_db.is_package_installed(package):
-            print(f"{' ' * indentation}{installed_color(package)}", end='')
+            self.__buffer.push(installed_color(package))
         else:
-            print(f"{' ' * indentation}{uninstalled_color(package)}", end='')
+            self.__buffer.push(uninstalled_color(package))
---
+++ src/ebuild_analyzer/output/output_buffer.py
@@ -0,0 +1,46 @@
+from contextlib import contextmanager
+
+
+class OutputBuffer:
+    __INDENTATION_INCREMENT = 4
+
+    def __init__(self):
+        self.__indentation = 0
+        self.__indentation_enabled = True
+        self.__buffer: str = ""
+
+    def print(self) -> None:
+        print(self.__buffer)
+
+    def push(self, data: str) -> None:
+        self.__buffer += data
+
+    def indented_push(self, data: str) -> None:
+        if self.__indentation_enabled:
+            self.__buffer += ' ' * self.__indentation
+        self.push(data)
+
+    def push_indent(self) -> None:
+        self.__indentation += self.__INDENTATION_INCREMENT
+
+    def pop_indent(self) -> None:
+        if self.__indentation > 0:
+            self.__indentation -= self.__INDENTATION_INCREMENT
+
+    def disable_indentation(self) -> None:
+        self.__indentation_enabled = False
+
+    def enable_indentation(self) -> None:
+        self.__indentation_enabled = True
+
+    @contextmanager
+    def disabled_indentation(self):
+        self.disable_indentation()
+        yield
+        self.enable_indentation()
+
+    @contextmanager
+    def scoped_indent(self):
+        self.push_indent()
+        yield
+        self.pop_indent()
