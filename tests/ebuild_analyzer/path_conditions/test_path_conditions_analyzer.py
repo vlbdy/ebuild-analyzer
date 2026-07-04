b5f30ffae94cd3d9a1d93ee3c07ca42a1c01001a
Vlad B <vbadyorny@gmail.com>
2026-07-04 14:01:45 +0300

    tests: Add extra test cases for path conditions analyzer

--- tests/ebuild_analyzer/path_conditions/test_path_conditions_analyzer.py
+++ tests/ebuild_analyzer/path_conditions/test_path_conditions_analyzer.py
@@ -32,6 +32,8 @@ def test_no_conditions(bash_parser: BashParser, path_conditions_analyzer: PathCo
     (b"use a && use b || optfeature",
      [PathCondition(disabled_use_flags=['a']), PathCondition(disabled_use_flags=['b'])]),
     (b"use a || use b && optfeature", [PathCondition(enabled_use_flags=['a']), PathCondition(enabled_use_flags=['b'])]),
+    (b"use a && use b || use c && optfeature",
+     [PathCondition(enabled_use_flags=['a', 'b']), PathCondition(enabled_use_flags=['c'])]),
 
     (b"use a && has_version pkg && optfeature", [PathCondition(enabled_use_flags=['a'], installed_packages=['pkg'])]),
 
@@ -82,6 +84,8 @@ def test_if_statement_conditions(bash: bytes, expected_path_conditions: List[Pat
      [PathCondition(enabled_use_flags=['a', 'b', 'c']), PathCondition(enabled_use_flags=['a', 'b', 'd'])]),
     (b"if use a || use b; then if use c && use d; then optfeature; fi; fi",
      [PathCondition(enabled_use_flags=['a', 'c', 'd']), PathCondition(enabled_use_flags=['b', 'c', 'd'])]),
+    (b"if use a && use b; then if use c && use d; then optfeature; fi; fi",
+     [PathCondition(enabled_use_flags=['a', 'b', 'c', 'd'])])
 ])
 def test_nested_if_statement_conditions(bash: bytes, expected_path_conditions: List[PathCondition],
                                         bash_parser: BashParser, path_conditions_analyzer: PathConditionsAnalyzer):
@@ -111,6 +115,10 @@ def test_if_conditions_mixed_with_short_circuit_conditions(bash: bytes, expected
     # The following test cases are similar to the test cases in `test_nested_if_statement_conditions`.
     # The difference is that those tests are technically also supposed to have the extra PathCondition added
     # to the end (where all the conditions in the OR'd commands are met).
+    (b"use a && use b && use c || use d && optfeature",
+     [PathCondition(enabled_use_flags=['a', 'c']), PathCondition(enabled_use_flags=['b', 'c']),
+      PathCondition(enabled_use_flags=['a', 'b', 'c'])]),
+
     (b"if use a; then if use b || use c; then optfeature; fi; fi",
      [PathCondition(enabled_use_flags=['a', 'b']), PathCondition(enabled_use_flags=['a', 'c']),
       PathCondition(enabled_use_flags=['a', 'b', 'c'])]),
@@ -124,6 +132,13 @@ def test_if_conditions_mixed_with_short_circuit_conditions(bash: bytes, expected
     (b"if use a || use b; then if use c && use d; then optfeature; fi; fi",
      [PathCondition(enabled_use_flags=['a', 'c', 'd']), PathCondition(enabled_use_flags=['b', 'c', 'd']),
       PathCondition(enabled_use_flags=['a', 'b', 'c', 'd'])]),
+
+    (b"if use a || use b; then if use c || use d; then optfeature; fi; fi",
+     [PathCondition(enabled_use_flags=['a', 'c']), PathCondition(enabled_use_flags=['a', 'd']),
+      PathCondition(enabled_use_flags=['b', 'c']), PathCondition(enabled_use_flags=['b', 'd']),
+      PathCondition(enabled_use_flags=['a', 'b', 'c']), PathCondition(enabled_use_flags=['a', 'b', 'd']),
+      PathCondition(enabled_use_flags=['a', 'c', 'd']), PathCondition(enabled_use_flags=['b', 'c', 'd']),
+      PathCondition(enabled_use_flags=['a', 'b', 'c', 'd'])])
 ])
 def test_unimplemented_cases(bash: bytes, expected_path_conditions: List[PathCondition], bash_parser: BashParser,
                              path_conditions_analyzer: PathConditionsAnalyzer):
