c88c1c2a59b7c48578c9f8c388d1ddfc51bbaffc
Vlad B <vbadyorny@gmail.com>
2026-07-04 17:17:37 +0300

    Make PackageAtom hashable

--- src/ebuild_analyzer/package_atoms/package_atom.py
+++ src/ebuild_analyzer/package_atoms/package_atom.py
@@ -2,8 +2,16 @@ from dataclasses import dataclass, field
 from typing import List
 
 
-@dataclass
+@dataclass(frozen=True)
 class PackageAtom:
     text: str
     required_enabled_use_flags: List[str] = field(default_factory=list)
     required_disabled_use_flags: List[str] = field(default_factory=list)
+
+    def __eq__(self, other: PackageAtom) -> bool:
+        return (self.text == other.text
+                and set(self.required_enabled_use_flags) == set(other.required_enabled_use_flags)
+                and set(self.required_disabled_use_flags) == set(other.required_disabled_use_flags))
+
+    def __hash__(self) -> int:
+        return hash((self.text, tuple(self.required_enabled_use_flags), tuple(self.required_disabled_use_flags)))
