516d641ef3860f944721472d411dd5ed32875a34
Vlad B <vbadyorny@gmail.com>
2026-09-06 22:51:32 +0300

    checked_kernel_config_printer: Print only relevant kernel config keys unless in verbose mode

--- src/ebuild_analyzer/output/printers/checked_kernel_config_printer.py
+++ src/ebuild_analyzer/output/printers/checked_kernel_config_printer.py
@@ -6,6 +6,7 @@ from ebuild_analyzer.kernel_config.local.local_kernel_config import LocalKernelC
 from ebuild_analyzer.output.ansi import Color, Format
 from ebuild_analyzer.output.printers.base_printer import BasePrinter
 from ebuild_analyzer.package_atoms.package_cpv import PackageCPV
+from ebuild_analyzer.path_conditions.path_conditions_evaluator import PathConditionsEvaluator
 from ebuild_analyzer.utils.conditional_message import ConditionalMessage
 from ebuild_analyzer.utils.portage_db import PortageDatabase
 
@@ -15,6 +16,7 @@ class CheckedKernelConfigPrinter(BasePrinter):
         super().__init__(portage_db, config.run_unknown_commands)
         self.__local_kernel_config = local_kernel_config
         self.__config = config
+        self.__path_conditions_evaluator = PathConditionsEvaluator(portage_db, config.run_unknown_commands)
 
     def print(self, package_cpv: PackageCPV, checked_kernel_config_keys: List[CheckedKernelConfigKey]) -> None:
         self._buffer.reset()
@@ -22,8 +24,16 @@ class CheckedKernelConfigPrinter(BasePrinter):
             Color.LIGHT_PURPLE(Format.BOLD(f"Checked kernel configuration for package {package_cpv.text}:\n")))
         self._buffer.push_indent()
 
-        enabled_kernel_config_keys = [key for key in checked_kernel_config_keys if key.enabled]
-        disabled_kernel_config_keys = [key for key in checked_kernel_config_keys if not key.enabled]
+        enabled_kernel_config_keys = [key for key in checked_kernel_config_keys if
+                                      key.enabled and self.__is_kernel_config_key_relevant(package_cpv, key)]
+        disabled_kernel_config_keys = [key for key in checked_kernel_config_keys if
+                                       not key.enabled and self.__is_kernel_config_key_relevant(package_cpv, key)]
+
+        if not enabled_kernel_config_keys and not disabled_kernel_config_keys:
+            self._buffer.indented_push(
+                "No relevant checked kernel configuration to show. (See all checked kernel configuration with the --verbose flag)\n")
+            self._buffer.print()
+            return
 
         if enabled_kernel_config_keys:
             self._buffer.indented_push(Color.BLUE("Kernel config keys to enable:\n"))
@@ -89,3 +99,7 @@ class CheckedKernelConfigPrinter(BasePrinter):
             self._buffer.indented_push("Printed when:\n")
             with self._buffer.scoped_indent():
                 self.print_colored_path_conditions(package_cpv, message.conditions)
+
+    def __is_kernel_config_key_relevant(self, package_cpv: PackageCPV, key: CheckedKernelConfigKey) -> bool:
+        return self.__path_conditions_evaluator.are_satisfied(package_cpv, key.conditional_requirements) \
+            or self.__config.verbose
---
+++ src/ebuild_analyzer/path_conditions/path_conditions_evaluator.py
@@ -0,0 +1,75 @@
+import subprocess
+from typing import List
+
+from ebuild_analyzer.kernel_version.local_kernel_version import get_local_kernel_version
+from ebuild_analyzer.package_atoms.package_cpv import PackageCPV
+from ebuild_analyzer.path_conditions.path_condition import PathCondition
+from ebuild_analyzer.utils.portage_db import PortageDatabase
+
+
+class PathConditionsEvaluator:
+    def __init__(self, portage_db: PortageDatabase, run_unknown_commands: bool) -> None:
+        self.__portage_db = portage_db
+        self.__run_unknown_commands = run_unknown_commands
+
+    def are_satisfied(self, package_cpv: PackageCPV, conditions: List[PathCondition]) -> bool:
+        for condition in conditions:
+            if not (self.are_enabled_use_flags_satisfied(package_cpv, condition)
+                    and self.are_disabled_use_flags_satisfied(package_cpv, condition)
+                    and self.are_installed_packages_satisfied(condition)
+                    and self.are_uninstalled_packages_satisfied(condition)
+                    and self.are_successful_commands_satisfied(condition)
+                    and self.are_failed_commands_satisfied(condition)
+                    and self.is_kernel_version_range_satisfied(condition)):
+                return False
+        return True
+
+    def are_enabled_use_flags_satisfied(self, package_cpv: PackageCPV, condition: PathCondition) -> bool:
+        for use_flag in condition.enabled_use_flags:
+            if not self.__portage_db.is_use_flag_enabled(package_cpv, use_flag):
+                return False
+        return True
+
+    def are_disabled_use_flags_satisfied(self, package_cpv: PackageCPV, condition: PathCondition) -> bool:
+        for use_flag in condition.disabled_use_flags:
+            if self.__portage_db.is_use_flag_enabled(package_cpv, use_flag):
+                return False
+        return True
+
+    def are_installed_packages_satisfied(self, condition: PathCondition) -> bool:
+        for package_atom in condition.installed_packages:
+            if not self.__portage_db.is_package_installed(package_atom):
+                return False
+        return True
+
+    def are_uninstalled_packages_satisfied(self, condition: PathCondition) -> bool:
+        for package_atom in condition.uninstalled_packages:
+            if self.__portage_db.is_package_installed(package_atom):
+                return False
+        return True
+
+    def are_successful_commands_satisfied(self, condition: PathCondition) -> bool:
+        if not self.__run_unknown_commands:
+            return False
+
+        for command in condition.successful_commands:
+            result = subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            if result != 0:
+                return False
+        return True
+
+    def are_failed_commands_satisfied(self, condition: PathCondition) -> bool:
+        if not self.__run_unknown_commands:
+            return False
+
+        for command in condition.failed_commands:
+            result = subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            if result == 0:
+                return False
+        return True
+
+    @staticmethod
+    def is_kernel_version_range_satisfied(condition: PathCondition) -> bool:
+        if condition.kernel_version_range is None:
+            return True
+        return condition.kernel_version_range.contains(get_local_kernel_version())
